f555f540290f9e4c421dec2659f057e3ff33c4d2
nasirmovlamov <60513066+nasirmovlamov@users.noreply.github.com>
2022-07-29 20:35:03 +0400

    builder pattern

---
+++ creational/builder.ts
@@ -0,0 +1,69 @@
+
+class Wall {
+  constructor() {
+    console.log('Wall built');
+  }
+}
+class Roof {
+  constructor() {
+    console.log('Roof built');
+  }
+}
+class Door {
+  constructor() {
+    console.log('Door built');
+  }
+}
+class homeWindow {
+  constructor() {
+    console.log('Window built');
+  }
+}
+class Home {
+  public wall?: Wall;
+  public roof?: Roof;
+  public door?: Door;
+  public homeWindow?: homeWindow;
+  constructor() {
+    
+  }
+}
+class HomeBuilder {
+  private home: Home;
+
+  constructor() {
+    this.home = new Home();
+  }
+
+  public buildWall(): HomeBuilder {
+    this.home.wall = new Wall();
+    return this;
+  }
+
+  public buildRoof(): HomeBuilder {
+    this.home.roof = new Roof();
+    return this;
+  }
+
+  public buildDoor(): HomeBuilder {
+    this.home.door = new Door();
+    return this;
+  }
+
+  public buildWindow(): HomeBuilder {
+    this.home.homeWindow = new homeWindow();
+    return this;
+  }
+
+  public build(): Home {
+    return this.home;
+  }
+}
+
+
+
+
+
+const newHome = new HomeBuilder();
+newHome.buildWall().buildDoor().build();
+console.log(newHome);
