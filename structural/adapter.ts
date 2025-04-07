8bd40b55165afeb57cf9beac86615952781771a2
Nasir Movlamov <60513066+nasirmovlamov@users.noreply.github.com>
2025-04-07 11:53:27 +0400

    corrected

--- structural/adapter.ts
+++ structural/adapter.ts
@@ -1,26 +1,30 @@
 class User {
-  constructor(private name: string) {}  
-    sayHi() {
-        console.log(`Hi, ${this.name}`);
-    }
-    getName() {
-        return this.name;
-    }
+  constructor(private name: string) {}
 
-    setName(name: string) {
-        this.name = name;
-    }
-  
+  sayHi() {
+    console.log(`Hi, ${this.name}`);
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
 }
 
+// Adapter Pattern
+class UserAdapter {
+  constructor(private user: User) {}
 
-class UserAdapter extends User {
-    constructor(name:any) {
-        super(name);
-    }
-    getFullName() {
-        return `${this.getName()}`;
-    }
+  getFullName() {
+    return `${this.user.getName()}`;
+  }
 }
 
+// Example Usage:
+const user = new User('John Doe');
+const userAdapter = new UserAdapter(user);
 
+console.log(userAdapter.getFullName()); // "John Doe"
